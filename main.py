b286251c4da3e68eeb5f195352134427ab10c7f4
rzyang <584688538@qq.com>
2021-05-17 18:24:46 +0800

    TODO: 上传文件后保持前端变化

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import os
 from flask import Flask, render_template
 from flask_login import LoginManager, current_user, login_required
 from user import User, get_user
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash, send_from_directory
 from flask_login import login_user
 from elements.LoginForm import LoginForm
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 # 上传文件路径
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'static', 'upload')
 
 # 创建Flask应用
 app = Flask(__name__)
@@ -33,6 +33,43 @@ login_manager.login_view = 'login'
 @login_required
 def index():
     return render_template('index.html', username=current_user.username)
+
+@app.route('/<weight_file>')
+@login_required
+def show_weight_file(weight_file):
+    return render_template('index.html', username=current_user.username, weight_file=weight_file)
+
+# 上传文件
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        print(type(request.files))
+        print(list(request.files.lists())[0])
+        file_type = request.form['type']
+        print(file_type)
+        if 'onnx_file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['onnx_file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(save_path)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return ''
+
+# 上传成功后的处理
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print(os.path.basename(filename))
+    return redirect(url_for('show_weight_file', weight_file=filename))
+    # return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/login/', methods=('GET', 'POST'))  # 登录
 def login():
